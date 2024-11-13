1606684cb19b804448ddafda1f0f39ed648ff030
LtMilligan <gioele.miscia@gmail.com>
2024-11-13 01:44:33 +0100

    [UPD]

--- src/store.js
+++ src/store.js
@@ -7,9 +7,5 @@ export const store = reactive({
     selectedPlan: { id: null }, // Cambia selectedPlan da null a un oggetto con id nullo
     apartmentsSponsored: [],
     currentApartment: {},
-<<<<<<< HEAD
-});
-=======
     filteredApartments: [],
 })
->>>>>>> 02330156a997c343f2230421ab39812273c1e489
