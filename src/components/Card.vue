777c3712c59bdd3753905bb1ff17715ec0a0528a
Rivio98 <rivaldogjoni1998@gmail.com>
2024-11-15 02:17:58 +0100

    [UPD]Add visuals on myapartments by ip adress

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,9 @@
 <template>
     <div class="content">
         <div class="card border-0">
-            <img
-                v-if="apartment.images.length > 0 && apartment.images[0].url"
-                :src="apartment.images[0].url"
-                class="card-img-top"
-                alt="Property Image" />
-            <span
-                v-if="apartment.unreadMessages && apartment.unreadMessages.length > 0"
+            <img v-if="apartment.images.length > 0 && apartment.images[0].url" :src="apartment.images[0].url"
+                class="card-img-top" alt="Property Image" />
+            <span v-if="apartment.unreadMessages && apartment.unreadMessages.length > 0"
                 class="position-absolute top-50 start-50 translate-middle badge rounded-pill bg-danger">
                 {{ apartment.unreadMessages.length }}
             </span>
@@ -16,32 +12,33 @@
                     {{ apartment.title }} <span class="city">{{ apartment.city }}</span>
                 </h2>
                 <p>{{ apartment.description }}</p>
-                <button class="button button-details px-2 py-1" @click="goToApartmentDetails(apartment)">
+                <button class="button button-details px-2 py-1" @click="goToApartmentDetails(apartment)"
+                    :disabled="isNavigating">
                     Scopri
                     <span class="material-symbols-outlined"> &rarr; </span>
                 </button>
             </div>
-            <button
-                v-if="showSponsorButton"
-                type="button"
-                class="position-absolute top-0 end-0 m-2"
-                @click="sponsor(apartment)"
-                data-bs-toggle="modal"
-                data-bs-target="#staticBackdrop">
+            <button v-if="showSponsorButton" type="button" class="position-absolute top-0 end-0 m-2"
+                @click="sponsor(apartment)" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
                 <span class="btn btn-warning" v-if="apartment.latest_package == null">
                     <i class="fa-solid fa-crown"></i> Sponsor
                 </span>
                 <span class="btn btn-danger" v-if="apartment.latest_package">
                     <i class="fa-solid fa-business-time"></i>
-                    {{ apartment.latest_package.pivot.sponsorship_end }}
+                    {{ formattedSponsorshipEnd }}
                 </span>
             </button>
+        </div>
+        <div v-if="store.isAuthenticated" class="view-count mt-5">
+            <p>Visualizzazioni: {{ apartmentViewCount }}</p>
         </div>
     </div>
 </template>
 
 <script>
-import { store } from "../store"
+import axios from "axios";
+import { store } from "../store";
+
 export default {
     props: {
         apartment: Object,
@@ -50,28 +47,72 @@ export default {
     data() {
         return {
             store,
-        }
+            isNavigating: false, // Per prevenire click multipli
+        };
     },
     computed: {
         formattedSponsorshipEnd() {
-            return this.apartment.latest_package?.pivot?.sponsorship_end.split(" ")[0]
+            return this.apartment.latest_package?.pivot?.sponsorship_end.split(" ")[0];
+        },
+        apartmentViewCount() {
+            return this.store.viewCounts[this.apartment.id] || 0;
         },
     },
-
     methods: {
         handleEditClick(apartment) {
-            this.store.apartmentToEdit = apartment
-            this.$router.push("/edit-apartment") // Naviga programmaticamente
+            this.store.apartmentToEdit = apartment;
+            this.$router.push("/edit-apartment");
         },
         sponsor(apartment) {
-            store.apartmentToSponsor = apartment
+            this.store.apartmentToSponsor = apartment;
         },
         goToApartmentDetails(apartment) {
-            store.currentApartment = apartment
-            this.$router.push({ name: "ApartmentDetails", params: { slug: apartment.slug } })
+            if (this.isNavigating) return;
+            this.isNavigating = true;
+
+            this.registerView(apartment.id)
+                .then(() => {
+                    this.store.currentApartment = apartment;
+                    this.$router.push({ name: "ApartmentDetails", params: { slug: apartment.slug } });
+                })
+                .finally(() => {
+                    this.isNavigating = false;
+                });
+        },
+        registerView(apartmentId) {
+            return axios
+                .post(`http://127.0.0.1:8000/api/apartments/${apartmentId}/views`, {
+                    apartment_id: apartmentId,
+                })
+                .then(() => {
+                    // Incrementa il conteggio nel tuo store
+                    if (this.store.viewCounts[apartmentId]) {
+                        this.store.viewCounts[apartmentId]++;
+                    } else {
+                        // Assicura la reattività aggiungendo la proprietà all'oggetto reattivo
+                        this.store.viewCounts = { ...this.store.viewCounts, [apartmentId]: 1 };
+                    }
+                })
+                .catch((error) => {
+                    console.error("Errore durante la registrazione della visualizzazione:", error);
+                });
+        },
+        fetchViewCounter(apartmentId) {
+            axios
+                .get(`http://127.0.0.1:8000/api/apartments/${apartmentId}/views`)
+                .then((response) => {
+                    const count = response.data.view_count;
+                    this.store.viewCounts = { ...this.store.viewCounts, [apartmentId]: count };
+                })
+                .catch((error) => {
+                    console.error("Errore durante la richiesta del conteggio delle visualizzazioni:", error);
+                });
         },
     },
-}
+    mounted() {
+        this.fetchViewCounter(this.apartment.id);
+    },
+};
 </script>
 
 <style scoped>
@@ -154,6 +195,7 @@ export default {
         text-decoration: none;
         border: 1px solid black;
         background-color: #ec622b;
+
         &:hover {
             background-color: #ff8858;
             color: black;
